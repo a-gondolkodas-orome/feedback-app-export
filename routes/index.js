d8dccad23339df7950b281c59d29f2bc28bdb4ff
Pituk Gabor <pitukgabor@gmail.com>
2020-02-26 23:30:49 +0000

    Set up Express with Jade.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'feedback-export' });
 });
 
 module.exports = router;

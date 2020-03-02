4d91fedcdd1133b074e1f24c00e943d16232f73e
Pituk Gabor <pitukgabor@gmail.com>
2020-03-02 16:41:38 +0000

    Created view structure.

--- routes/export.js
+++ routes/export.js
@@ -1,70 +1,34 @@
-var firebase = require("firebase");
+var data = require('../data');
 var express = require('express');
 var router = express.Router();
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDmshJPEN5RyoQB5HI9f6vf4Ys7dn8Gkkw",
-  projectId: "feedback-app-ago",
-  authDomain: "feedback-app-ago.firebaseapp.com",
-  databaseURL: "https://feedback-app-ago.firebaseio.com",
-  storageBucket: "feedback-app-ago.appspot.com"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-// Get a database reference to our posts
-var db = firebase.firestore();
-
-const table = db.collection("events").doc("mamut-2019-szervezo").collection("questions");
-const events = db.collection('events');
-
-
-// Retrieve data from firebase
-
-let answers = {}
-
-events.get()
-  .then((_events) => {
-    _events.forEach((eventDoc) => {
-      answers[eventDoc.id] = {};
-      questionCollection = events.doc(eventDoc.id).collection('questions');
-      questionCollection.get()
-        .then((_questions) => {
-          _questions.forEach((questionDoc) => {
-            answers[eventDoc.id][questionDoc.id] = [];
-            questionCollection.doc(questionDoc.id).collection('answers').get()
-              .then((_answers) => {
-                _answers.forEach((answerDoc) => {
-                  answers[eventDoc.id][questionDoc.id].push(answerDoc.data());
-                })
-              })
-          })
-        })
-    });
-  });
-
 // TODO: JSON to CSV
 
-/* GET answers listing. */
+// GET events listing.
 router.get('/', function(req, res, next) {
+  answers = data.getAnswers();
   res.render('events', {
     events: answers
   });
 });
 
+// GET questions listing.
 router.get('/:eventId', function(req, res, next) {
+  answers = data.getAnswers();
   if (answers.hasOwnProperty(req.params['eventId'])) {
     res.render('questions', {
       eventId: req.params['eventId'],
       questions: answers[req.params['eventId']]
     });
   } else {
+    // Not found
     next();
   }
 });
 
+// GET answers listing.
 router.get('/:eventId/:questionId', function(req, res, next) {
+  answers = data.getAnswers();
   if (answers.hasOwnProperty(req.params['eventId'])
    && answers[req.params['eventId']].hasOwnProperty(req.params['questionId'])) {
     res.render('answers', {
@@ -73,11 +37,9 @@ router.get('/:eventId/:questionId', function(req, res, next) {
       answers: answers[req.params['eventId']][req.params['questionId']]
     });
   } else {
+    // Not found
     next();
   }
 });
 
 module.exports = router;
-
-
-
